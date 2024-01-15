8dcf2c2c4e6d7b4a67400e165b96a37749fa2e3b
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-15 18:22:25 +0100

    Clear Git cache

--- server/Index.js
+++ server/Index.js
@@ -1,18 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
 dotenv.config()
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT =  3000;
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 
-app.listen(PORT , ()=>{
-    console.log('Server is running on port' + PORT)
-})
+app.listen(PORT, ()=> {
+    connect()
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+const connect = async()=>{
+    try {
+
+    await mongoose.connect(process.env.MONGO);
+console.log('MongoDb connected ');
+} catch (error) {
+   
+    console.log(error) 
+}
+}
