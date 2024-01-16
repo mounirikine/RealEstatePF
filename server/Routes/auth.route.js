02ae7c46fd66b3ba43cf278699ee3ca24c01accf
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-17 00:20:06 +0100

    c

--- server/Routes/auth.route.js
+++ server/Routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import { SignIn, SignUp, google, signOut } from '../Controllers/auth.controller.js';
+import { SignIn, SignUp, UpdateUser,  signOut } from '../Controllers/auth.controller.js';
 
 
 
 const router = express.Router()
 
-router.post("/google",google);
 router.post('/sign-up',SignUp)
 router.post('/sign-in',SignIn)
 router.get("/signout",signOut);
