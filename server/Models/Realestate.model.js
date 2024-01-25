2fcfa7f70937f6456589532e1a6957e70096eb62
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-25 17:56:36 +0100

    home villa appart office / model

---
+++ server/Models/Realestate.model.js
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose';
+
+const RealestateSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    bathrooms: {
+      type: Number,
+      required: false,
+    },
+    rooms: {
+      type: Number,
+      required: true,
+    },
+    furnished: {
+      type: Boolean,
+      required: true,
+    },
+    parking: {
+      type: Boolean,
+      required: true,
+    },
+    parkSpace: {
+        type: Number,
+        required: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    catSlug: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: String,
+        required: true,
+    },
+    zip: {
+        type: String,
+        required: false,
+    },
+    kitchen: {
+        type: Boolean,
+        required: false,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Real = mongoose.model('Real', RealestateSchema);
+
+export default Real;
