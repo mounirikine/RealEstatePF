b194c90cbcefb841c7ee23e1f1997be727c32b75
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-01-25 21:41:03 +0100

    delete real api

--- server/Controllers/Real.controller.js
+++ server/Controllers/Real.controller.js
@@ -14,3 +14,20 @@ export const createReal = async(req,res,next)=>{
     }
 }
 
+
+export const deleteReal = async(req,res,next)=>{
+    const listing = await Real.findById(req.params.id);
+ if(!listing){
+     return next(errorHandler(404,'Real not found1'));
+ }
+ if(req.user.id !== listing.userRef){
+     return next(errorHandler(401,'You can only delete your own Real!'));
+ }
+ try {
+ 
+     await Real.findByIdAndDelete(req.params.id);
+ res.status(200).json('listings has been deleleted!')
+ } catch (error) {
+     next(error);
+ }
+ }
--- server/Routes/Real.route.js
+++ server/Routes/Real.route.js
@@ -1,12 +1,12 @@
 import express  from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createReal } from "../Controllers/Real.controller.js";
+import { createReal, deleteReal } from "../Controllers/Real.controller.js";
 const router = express.Router();
 
 
 
 
 router.post('/create-real',verifyToken ,createReal)
-
+router.delete('/delete-real/:id',verifyToken ,deleteReal)
 
 export default router;
