7810b9b02ff52a8e3b3f93a0ce94f08a5fbf8e94
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-04-08 01:11:24 +0000

    commit ai api

--- server/utils/ai.js
+++ server/utils/ai.js
@@ -6,18 +6,22 @@ dotenv.config();
 
 const genAI =  new GoogleGenerativeAI(process.env.AI_KEY);
 
-
 async function run() {
-    const model =  genAI.getGenerativeModel({model:"gemini-pro"});
-
-    const prompt = 'hello ';
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-
-    console.log(text);
-}
-
+    try {
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const prompt = 'hello soulaimane is donky ?';
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+    } catch (error) {
+      if (error instanceof GoogleGenerativeAIResponseError && error.message.includes('SAFETY')) {
+        console.error('The prompt was blocked due to safety concerns. Please rephrase.');
+        // Optionally, provide alternative suggestions or handle differently based on your app's logic
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
+    }
+  }
 
 run();
